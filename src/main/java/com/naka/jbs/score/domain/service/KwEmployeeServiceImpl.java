52cbba4386d1c5d0ee8e93c5f6a4029be3963fd4
eifaookfue <s7.nakamura@gmail.com>
2025-03-22 17:24:44 +0900

    postKwEmployeeメソッドの実装追加

--- src/main/java/com/naka/jbs/score/domain/service/KwEmployeeServiceImpl.java
+++ src/main/java/com/naka/jbs/score/domain/service/KwEmployeeServiceImpl.java
@@ -1,9 +1,13 @@
 package com.naka.jbs.score.domain.service;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.naka.jbs.score.domain.model.entity.score.KwEmployee;
 import com.naka.jbs.score.domain.repository.score.KwEmployeeRepository;
 
@@ -14,10 +18,22 @@ import lombok.RequiredArgsConstructor;
 public class KwEmployeeServiceImpl implements KwEmployeeService {
 
     private final KwEmployeeRepository kwEmployeeRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
 
     @Override
     public List<KwEmployee> getAll() {
         return kwEmployeeRepository.findAll();
+    }
+
+    @Override
+    public void postKwEmployee() {
+        getAll().forEach(kwEmployee -> {
+            Map<String, Object> map = objectMapper.convertValue(kwEmployee, new TypeReference<Map<String, Object>>() {});
+            String key = "kwEmployee" + ":" + kwEmployee.getUid();
+            redisTemplate.opsForHash().putAll(key, map);
+        });
+
     }
 
 }
