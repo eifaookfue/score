fb9707f3df1e10bba1ccbe88c1b67b8d67799013
eifaookfue <s7.nakamura@gmail.com>
2025-03-22 16:12:50 +0900

    KwEmployeeServiceを追加

---
+++ src/main/java/com/naka/jbs/score/domain/service/KwEmployeeService.java
@@ -0,0 +1,10 @@
+package com.naka.jbs.score.domain.service;
+
+import java.util.List;
+
+import com.naka.jbs.score.domain.model.entity.score.KwEmployee;
+
+public interface KwEmployeeService {
+
+    List<KwEmployee> getAll();
+}
