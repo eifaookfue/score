b54614df815b487f27921483f76b200c9f60090d
eifaookfue <s7.nakamura@gmail.com>
2025-03-23 17:47:35 +0900

    DL_USERのDML追加

---
+++ src/main/resources/db/migration/V4__insert_dluser_data.sql
@@ -0,0 +1,2 @@
+insert into dl_user(dl_name, uid, updated_time) values('IB', 'nakamura', CURRENT_TIMESTAMP);
+insert into dl_user(dl_name, uid, updated_time) values('GM', 'ozaki', CURRENT_TIMESTAMP);
