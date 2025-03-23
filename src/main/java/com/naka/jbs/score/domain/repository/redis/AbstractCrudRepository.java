c848bcd8b8feafda7b0918c93446d63227115350
eifaookfue <s7.nakamura@gmail.com>
2025-03-23 19:33:44 +0900

    saveにEmbeddedIdの考慮追加

--- src/main/java/com/naka/jbs/score/domain/repository/redis/AbstractCrudRepository.java
+++ src/main/java/com/naka/jbs/score/domain/repository/redis/AbstractCrudRepository.java
@@ -1,13 +1,17 @@
 package com.naka.jbs.score.domain.repository.redis;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Id;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +29,23 @@ public abstract class AbstractCrudRepository<T, ID> implements CrudRepository<T,
 
     @Override
     public <S extends T> void save(S entity) {
-        Map<String, Object> map = objectMapper.convertValue(entity, new TypeReference<Map<String, Object>>() {});
+        Map<String, Object> map = convertValue(entity);
         String entityName = toLowerCase(entity.getClass().getSimpleName());
-        String id = getId(entity);
+        String id = getId(entity).or(() -> getEmbeddedId(entity)).orElseThrow(() -> new IllegalArgumentException("Entityには@Idか@Embeddableのどちらかを設定したフィールドが必要です"));
         String key = entityName + ":" + id;
         redisTemplate.opsForHash().putAll(key, map);
         redisTemplate.opsForSet().add("__" + entityName, id);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <S extends T> Map<String, Object> convertValue(S entity) {
+        Map<String, Object> map = objectMapper.convertValue(entity, new TypeReference<Map<String, Object>>() {});
+        return map.entrySet().stream().flatMap(e -> {
+            if (e.getValue() instanceof Map) {
+                return ((Map<String, Object>) e.getValue()).entrySet().stream();
+            }
+            return Stream.of(e);
+        }).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (x, y) -> x, LinkedHashMap::new));
     }
 
     private String toLowerCase(String text) {
@@ -39,15 +54,23 @@ public abstract class AbstractCrudRepository<T, ID> implements CrudRepository<T,
         return new String(c);
     }
 
-    // TODO: Embeddableの考慮
-    private <S extends T> String getId(S entity) {
-        return getIdField(getEntityClass()).map(f -> this.<String>getValue(f, entity)).get();
+    private <S extends T> Optional<String> getId(S entity) {
+        return getAnnotationField(getEntityClass(), Id.class).map(f -> this.<String>getValue(f, entity));
     }
 
-    private Optional<Field> getIdField(Class<?> clazz) {
+    private <S extends T> Optional<String> getEmbeddedId(S entity) {
+        return getAnnotationField(getEntityClass(), EmbeddedId.class).map(f -> this.getValue(f, entity))
+                .map(o -> getAllFields(o).map(Object::toString).collect(Collectors.joining(":")));
+    }
+
+    private Optional<Field> getAnnotationField(Class<?> clazz, Class<? extends Annotation> annotationClass) {
         return Arrays.stream(clazz.getDeclaredFields())
-                .filter(field -> field.isAnnotationPresent(Id.class))
+                .filter(field -> field.isAnnotationPresent(annotationClass))
                 .findFirst();
+    }
+
+    private <V> Stream<V> getAllFields(Object object) {
+        return Arrays.stream(object.getClass().getDeclaredFields()).map(f -> getValue(f, object));
     }
 
     @SuppressWarnings("unchecked")
